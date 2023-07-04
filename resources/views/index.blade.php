56773f47db05e50679d00e7719f321d48dd60521
vincenzodivita <divitavincenzo@icloud.com>
2023-07-04 12:22:18 +0200

    Rename view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,6 @@
         @vite('resources/js/app.js')
     </head>
     <body>
-        <h1 class="text-center">Ciao da vite</h1>
-        {{-- <img src="{{ Vite::asset('resources/img/picsum30.jpg') }}" alt=""> --}}
+
     </body>
 </html>
