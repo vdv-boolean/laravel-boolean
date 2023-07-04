cc1d56159f8f9f87c4002e25684706c9322c7d57
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-04 11:39:22 +0200

    add: seeders

---
+++ database/seeders/TravelsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TravelsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach (config('travels') as $travel) {
+
+            Travel::create([]);
+        }
+    }
+}
