e39c97cdca352c9537de426145e68a31b110db10
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-07-04 15:45:56 +0200

    add yield

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-        @vite('resources/js/app.js')
-    </head>
-    <body>
 
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <div class="container">
+        @yield('contents')
+    </div>
+
+</body>
+
 </html>
