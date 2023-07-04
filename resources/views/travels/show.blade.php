32b185ccec49d833d7cc0d792e01049ca7e7bc68
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-07-04 17:53:17 +0200

    add edit button2

--- resources/views/travels/show.blade.php
+++ resources/views/travels/show.blade.php
@@ -7,5 +7,8 @@
         <h2>{{ $travel->country }}</h2>
         <p>{{ $travel->text }}</p>
         <h3>{{ $travel->date }}</h3>
+        <a class="btn
+        btn-warning" href="{{ route('travels.edit', ['travel' => $travel->id]) }}">EDIT
+        </a>
     </div>
 @endsection
