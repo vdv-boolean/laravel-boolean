6646fbbd2f3a86ad4aae90285b7ab17d329ff0c3
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-07-04 12:14:05 +0200

    fixed TravelController requests values

--- app/Http/Controllers/Guest/TravelController.php
+++ app/Http/Controllers/Guest/TravelController.php
@@ -34,10 +34,10 @@ class TravelController extends Controller
         $data = $request->all();
 
         $newTravels = new Travel();
-        $newTravels->title = $data['title'];
         $newTravels->date = $data['date'];
-        $newTravels->description = $data['description'];
-        $newTravels->thumb = $data['thumb'];
+        $newTravels->title = $data['title'];
+        $newTravels->text = $data['text'];
+        $newTravels->image = $data['image'];
         $newTravels->country = $data['country'];
         $newTravels->address = $data['address'];
         $newTravels->save();
@@ -61,25 +61,23 @@ class TravelController extends Controller
     {
         // Validate the data
         $request->validate([
+            'date'           => 'required|string|max:100',
             'title'           => 'required|string|max:200',
-            'price'           => 'required|string|max:100',
-            'series'          => 'required|string|max:100',
-            'sale_date'       => 'required|date',
-            'type'            => 'required|string|max:100',
-            'description'     => 'required|string|max:1000',
-            'thumb'           => 'required|string|max:1000',
+            'text'          => 'required|string|max:100',
+            'image'       => 'required|date',
+            'country'            => 'required|string|max:100',
+            'address'     => 'required|string|max:1000',
         ]);
 
         $data = $request->all();
 
         // Update the data in the database
         $travel->title = $data['title'];
-        $travel->price = $data['price'];
-        $travel->series = $data['series'];
-        $travel->sale_date = $data['sale_date'];
-        $travel->type = $data['type'];
-        $travel->description = $data['description'];
-        $travel->thumb = $data['thumb'];
+        $travel->date = $data['date'];
+        $travel->text = $data['text'];
+        $travel->image = $data['image'];
+        $travel->country = $data['country'];
+        $travel->address = $data['address'];
         $travel->update();
 
         return redirect()->route('travels.show', ['travel' => $travel->id]);
