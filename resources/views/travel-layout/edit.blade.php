7becbdf9b33a07268970bd7b4b97bc68e5bbed15
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-07-04 15:41:59 +0200

    edit label

--- resources/views/travel-layout/edit.blade.php
+++ resources/views/travel-layout/edit.blade.php
@@ -13,35 +13,35 @@
             </div>
         </div>
         <div class="mb-3">
-            <label for="date" class="form-label">Titolo</label>
+            <label for="date" class="form-label">Data</label>
             <input type="text" class="form-control @error('date') is-invalid @enderror" id="date" name="date" value="{{ old('date', $travel->date) }}">
             <div class="invalid-feedback">
                 @error('date') {{ $message }} @enderror
             </div>
         </div>
         <div class="mb-3">
-            <label for="text" class="form-label">Titolo</label>
+            <label for="text" class="form-label">Testo</label>
             <input type="text" class="form-control @error('text') is-invalid @enderror" id="text" name="text" value="{{ old('text', $travel->text) }}">
             <div class="invalid-feedback">
                 @error('text') {{ $message }} @enderror
             </div>
         </div>
         <div class="mb-3">
-            <label for="image" class="form-label">Titolo</label>
+            <label for="image" class="form-label">Immagine</label>
             <input type="text" class="form-control @error('image') is-invalid @enderror" id="image" name="image" value="{{ old('image', $travel->image) }}">
             <div class="invalid-feedback">
                 @error('image') {{ $message }} @enderror
             </div>
         </div>
         <div class="mb-3">
-            <label for="country" class="form-label">Titolo</label>
+            <label for="country" class="form-label">Nazione</label>
             <input type="text" class="form-control @error('country') is-invalid @enderror" id="country" name="country" value="{{ old('country', $travel->country) }}">
             <div class="invalid-feedback">
                 @error('country') {{ $message }} @enderror
             </div>
         </div>
         <div class="mb-3">
-            <label for="address" class="form-label">Titolo</label>
+            <label for="address" class="form-label">Indirizzo</label>
             <input type="text" class="form-control @error('address') is-invalid @enderror" id="address" name="address" value="{{ old('address', $travel->address) }}">
             <div class="invalid-feedback">
                 @error('address') {{ $message }} @enderror
