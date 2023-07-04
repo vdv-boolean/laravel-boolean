a4c29006a4c8036f4201baa0a002fc6e2a374a58
vincenzodivita <divitavincenzo@icloud.com>
2023-07-04 16:46:33 +0200

    Fix

--- resources/views/travels/show.blade.php
+++ resources/views/travels/show.blade.php
@@ -5,7 +5,7 @@
         <img src="{{ $travel->image }}" alt="">
         <h1>{{ $travel->title }}</h1>
         <h2>{{ $travel->country }}</h2>
-        <p>{!! $travel->text !!}</p>
+        <p>{{ $travel->text }}</p>
         <h3>{{ $travel->date }}</h3>
     </div>
 @endsection
