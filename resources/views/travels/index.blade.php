d1f6194c3aee7a5b0604ee4cffcf4c596bb32706
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-07-04 17:40:07 +0200

    add show button and cards

--- resources/views/travels/index.blade.php
+++ resources/views/travels/index.blade.php
@@ -3,12 +3,26 @@
 @section('contents')
     <h1 class="text-center m-5">My Travels Diary</h1>
 
-    <div class="container">
-        <div class="book">
-            <p>Hello</p>
-            <div class="cover">
-                <p>Hover Me</p>
+    <div class="container d-flex gap-5">
+        @foreach ($travels as $travel)
+            <div class="card">
+                <img src="{{ $travel->image }}" alt="">
+
+                <div class="card__content">
+                    <p class="card__title">{{ $travel->title }}</p>
+                    <p class="card__description">{{ $travel->date }}
+                    </p>
+                    <p class="card__description">{{ $travel->country }}
+                    </p>
+                    <p class="card__description">{{ $travel->address }}
+                    </p>
+                    <a class="btn
+                        btn-primary"
+                        href="{{ route('travels.show', ['travel' => $travel->id]) }}">SHOW
+                    </a>
+                </div>
             </div>
-        </div>
+        @endforeach
+
     </div>
 @endsection
